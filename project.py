1199417faca55d883c184270476e1d532a189c4d
Janet Mardjuki <mardjuki@sfu.ca>
2019-11-09 21:41:25 -0800

    updated project.py

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ import numpy as np
 import json
 import re
 import string
+import ast
 from datetime import datetime, timedelta
 
 def parseJson(jason):
@@ -17,6 +18,11 @@ def roundToGrid(n):
 def elapsedTime(startTime, elapsedMinutes):
 	d = timedelta(minutes=elapsedMinutes)
 	return startTime + d
+
+def toOneHot(row):
+	for h in row[3]:
+		row[5 + h] = 1
+
 
 def main():
 	parkingMeters_df = pd.read_csv("./data/parking-meters.csv", ";", dtype = {'PAY_PHONE': str, 'Geom': str}, usecols = ["PAY_PHONE", "Geom"])
@@ -34,23 +40,31 @@ def main():
 	data['MINUTES'] = pd.to_numeric(data['MINUTES'])
 
 	# FOR DEV, SO WE DON'T WAIT AN HOUR PER ATTEMPT
-	data = data.head(10000)
+	# data = data.head(1000)
 
 
 	data["DATE START"] = pd.to_datetime(data["DATE START"])
-	
 	data["DATE START"] = data["DATE START"].map(lambda x: x.replace(second = 0))
 	data["DATE END"] = data.apply(lambda x: elapsedTime(x["DATE START"], x['MINUTES']), axis=1)
+	data["START HOUR"] = data["DATE START"].map(lambda x: x.hour)
+	data["END HOUR"] = data["DATE END"].map(lambda x: x.hour)
+	data['HOURS'] = [[hour for hour in range(1, 24) if t[0] <= hour and hour <= t[1]] for t in zip(data['START HOUR'], data['END HOUR'])]
 	data["WEEKDAY"] = data["DATE START"].map(lambda x: x.weekday())
-	data["HOUR"] = data["DATE START"].map(lambda x: x.hour)
 	data['lat'] = roundToGrid(data['lat'])
 	data['lon'] = roundToGrid(data['lon'])
-	data = data.drop(['Geom'], 1)
+	data = data.drop(['Geom', 'DATE START', 'DATE END', 'MINUTES', 'START HOUR', 'END HOUR'], 1)
 
-	
+	for i in range(0, 24):
+		data[str(i)] = 0
 
-	print(data)
+	dlist = data.values.tolist()
+	for r in dlist:
+		toOneHot(r)
 
+	data2 = pd.DataFrame(dlist, columns=list(data.columns))
+	data2.drop('HOURS')
+	data.to_csv("./data/1.onehots")
+	# print(data2)
 
 
 if __name__=='__main__':
